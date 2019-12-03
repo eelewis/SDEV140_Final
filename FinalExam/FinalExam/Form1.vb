a98a467a0cfb0703305711d8f0a64168972f7926
Edward Lewis <37776754+eelewis@users.noreply.github.com>
2019-12-03 08:16:58 -0500

    Uploaded initial project files
    This is V1 of the Final Exam for SDEV 140. The basic functionality required is all implemented. No additional features or input verification/error handling have been added.

---
+++ FinalExam/FinalExam/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox(InputBox.Text * 2)
+        InputBox.Text = ""
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MsgBox(InputBox.Text / 2)
+        InputBox.Text = ""
+    End Sub
+End Class
